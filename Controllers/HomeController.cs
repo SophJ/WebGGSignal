7779360c2ab75f086dec15f71ddfe18afa6e21ba
SophJ <yixin.sophie@gmail.com>
2020-08-12 18:26:19 +0800

    123
    TESTING2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -452,7 +452,7 @@ namespace WebGGSignal.Controllers
 
                 CultureInfo enUK = new CultureInfo("en-UK");
                 //CultureInfo enSG = new CultureInfo("en-SG");
-                TimeZoneInfo sgtZone = TimeZoneInfo.FindSystemTimeZoneById("Singapore Standard Time");
+                TimeZoneInfo sgtZone = TimeZoneInfo.FindSystemTimeZoneById("Asia/Singapore");   //Singapore Standard Time
 
                 string lstdateString, format;
 
